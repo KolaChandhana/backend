260aff57cbc9ef2848ebd54faeafc973b88daa9f
KolaChandhana <kolachandhana@gmail.com>
2026-06-01 16:53:06 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -234,10 +234,11 @@ const loginUser = async (req, res) => {
             },
         });
     } catch (error) {
-        res.status(500).json({
-            message: error.message,
-        });
-    }
+    console.error("LOGIN ERROR:", error);
+    res.status(500).json({
+        message: error.message,
+    });
+}
 };
 module.exports = {
     registerUser,
