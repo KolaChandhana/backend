e16960913c0ecaf80019413c8e5f5b362a47df57
KolaChandhana <kolachandhana@gmail.com>
2026-06-01 17:03:11 +0530

    backend

--- server.js
+++ server.js
@@ -2,25 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
 const authRoutes = require("./routes/authRoutes");
 const recipeRoutes = require("./routes/recipeRoutes");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/api/auth", authRoutes);
+app.use("/api/recipes", recipeRoutes);
+app.get("/", (req, res) => {
+    res.send("Recipe API Running");
+});
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("MongoDB Connected"))
 .catch((err) => {
     console.error("MongoDB connection failed:", err);
     process.exit(1);
 });
-app.use("/api/auth", authRoutes);
-app.use("/api/recipes", recipeRoutes);
-app.get("/", (req, res) => {
-    res.send("Recipe API Running");
-});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-const path = require("path");
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
